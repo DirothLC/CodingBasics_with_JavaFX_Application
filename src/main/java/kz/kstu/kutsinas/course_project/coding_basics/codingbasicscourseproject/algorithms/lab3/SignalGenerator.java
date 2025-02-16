b10832fe953cb48e15b05944dd94eecdff67ceff
Kutsinas Darius <dkuc222@gmail.com>
2025-02-16 13:03:14 +0500

    Charts view management

--- src/main/java/kz/kstu/kutsinas/course_project/coding_basics/codingbasicscourseproject/algorithms/lab3/SignalGenerator.java
+++ src/main/java/kz/kstu/kutsinas/course_project/coding_basics/codingbasicscourseproject/algorithms/lab3/SignalGenerator.java
@@ -5,6 +5,7 @@ import javafx.scene.chart.XYChart;
 public class SignalGenerator {
     public static XYChart.Series<Number, Number> generateSignal(double frequency) {
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
         double samplingRate = 1000; // Частота дискретизации (Гц)
         double duration = 1.0; // Длительность сигнала (1 секунда)
         int points = (int) (samplingRate * duration);
--- src/main/java/kz/kstu/kutsinas/course_project/coding_basics/codingbasicscourseproject/controllers/lab3/Lab3Controller.java
+++ src/main/java/kz/kstu/kutsinas/course_project/coding_basics/codingbasicscourseproject/controllers/lab3/Lab3Controller.java
@@ -2,13 +2,17 @@ package kz.kstu.kutsinas.course_project.coding_basics.codingbasicscourseproject.
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import kz.kstu.kutsinas.course_project.coding_basics.codingbasicscourseproject.algorithms.lab3.Lab3Logic;
+
 
 public class Lab3Controller {
 
     public Lab3Controller() {
     }
+    @FXML
+    private Button runButton;
 
     @FXML
     private TextField frequencyInput;
@@ -25,10 +29,13 @@ public class Lab3Controller {
     @FXML
     private LineChart<Number,Number> restoredGraph;
 
+
     @FXML
     protected void onButtonRunClick(){
-        double frequency = Double.parseDouble(frequencyInput.getText());
-        double noise = Double.parseDouble(noiseInput.getText());
+        double frequency = 20;
+        double noise = 0.1;
+        if(!frequencyInput.getText().isEmpty()) frequency=Double.parseDouble(frequencyInput.getText());
+        if(!noiseInput.getText().isEmpty()) noise= Double.parseDouble(noiseInput.getText());
 
         var initialSignal = Lab3Logic.generateInitialSignal(frequency);
         var distortedSignal = Lab3Logic.generateDistortedSignal(initialSignal, noise);
@@ -36,12 +43,18 @@ public class Lab3Controller {
 
         initialGraph.getData().clear();
         initialGraph.getData().add(initialSignal);
+        initialGraph.setCreateSymbols(false);
+        initialGraph.setLegendVisible(false);
 
         distortedGraph.getData().clear();
         distortedGraph.getData().add(distortedSignal);
+        distortedGraph.setCreateSymbols(false);
+        distortedGraph.setLegendVisible(false);
 
         restoredGraph.getData().clear();
         restoredGraph.getData().add(restoredSignal);
+        restoredGraph.setCreateSymbols(false);
+        restoredGraph.setLegendVisible(false);
     }
 
     @FXML
